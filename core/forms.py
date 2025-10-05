6cc4826ec0f156519f1a60d5bdbd8b7adb24a344
dunantj <joel.dunant@gmail.com>
2025-10-05 10:13:30 +0200

    fix_userform

--- core/forms.py
+++ core/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import HomeTeam, Profile, Offer, VolunteerSlot
 
+# --- SIGNUP FORM ---
 class CustomSignupForm(UserCreationForm):
     email = forms.EmailField(required=True)
     first_name = forms.CharField(max_length=30, required=True)
@@ -32,9 +33,10 @@ class CustomSignupForm(UserCreationForm):
         user.email = self.cleaned_data["email"]
         user.first_name = self.cleaned_data["first_name"]
         user.last_name = self.cleaned_data["last_name"]
+
         if commit:
             user.save()
-            # Save phone number and home_team to profile
+            # Save profile fields
             profile = user.profile
             profile.phone_number = self.cleaned_data.get("phone_number", "")
             home_team = self.cleaned_data.get("home_team")
@@ -43,35 +45,25 @@ class CustomSignupForm(UserCreationForm):
             profile.save()
         return user
 
-class ProfileForm(forms.ModelForm):
-    first_name = forms.CharField(max_length=30, required=True)
-    last_name = forms.CharField(max_length=30, required=True)
 
-    class Meta:
-        model = Profile
-        fields = ("phone_number", "home_team")
-
-    def __init__(self, *args, **kwargs):
-        user = kwargs.pop('user', None)
-        super().__init__(*args, **kwargs)
-        if user:
-            self.fields['first_name'].initial = user.first_name
-            self.fields['last_name'].initial = user.last_name
-
-    def save(self, commit=True):
-        profile = super().save(commit=False)
-        user = profile.user
-        user.first_name = self.cleaned_data["first_name"]
-        user.last_name = self.cleaned_data["last_name"]
-        if commit:
-            user.save()
-            profile.save()
-        return profile
-
+# --- USER EDIT FORM ---
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email']  # let user edit these
+        fields = ['first_name', 'last_name', 'email']
+
+
+# --- PROFILE EDIT FORM ---
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['phone_number', 'home_team']
+
+    def save(self, commit=True):
+        profile = super().save(commit=False)
+        if commit:
+            profile.save()
+        return profile
 
 class OfferForm(forms.ModelForm):
     class Meta:
